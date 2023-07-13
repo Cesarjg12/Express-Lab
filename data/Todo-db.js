27c2fb1ddb50d82e442f6a717849d782842115fe
cesarjg28 <cesarjg12@yahoo.com>
2023-07-13 02:59:35 -0700

    added the correct missing files

--- data/Todo-db.js
+++ data/Todo-db.js
@@ -3,7 +3,7 @@ const todos = [
     {task: 'Feed pets', done: false},
     {task: 'Eat breakfast', done: false},
     {task: 'Get ready', done: false},
-    {task: 'Attend Class', done: false}
+    {task: 'Attend Class', done: true}
   ];
 
 
--- server.js
+++ server.js
@@ -2,26 +2,25 @@
 // Require modules
 const express = require('express');
 const path = require('path');
-
-// Create the Express app
+const app = express();
+  
 
 const todoDb = require('./data/Todo-db');
 
-const app = express();
-  
 // Configure the app (app.set)
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
   
-// Mount middleware (app.use)
-  
-  
-// Mount routes
-// app.get('/', function (req, res) {
-//   res.send('<h1>Hello Express! Its me Cesar!</h1>');
-// });
-  
 
+app.get('/home', function (req, res) {
+  res.render('home')
+})
+
+
+
+app.get('/', function(req, res) {
+  res.redirect('/todos');
+});
 
 app.get('/todos', function(req, res) {
     res.render('todos/index', {
@@ -29,11 +28,6 @@ app.get('/todos', function(req, res) {
     });
   });
 
-
-
-  app.get('/', function(req, res) {
-    res.redirect('/todos');
-  });
 
 // Tell the app to listen on port 3000
 app.listen(3000, function() {
